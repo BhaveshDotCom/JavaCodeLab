6b154d15288c369bcac4a36feb38251d84501b36
BhaveshDotCom <mail.bhavesh.in@gmail.com>
2026-05-19 14:33:12 +0530

    feat(ConsoleIO): add ConsoleIO class with main method and MyPrintStream functionality

---
+++ StandardIO/ConsoleIO.java
@@ -0,0 +1,21 @@
+package StandardIO;
+
+public class ConsoleIO {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        MySystem.MyOut.printOne();
+    }
+    
+}
+
+// Under The Hood
+class MyPrintStream {
+    public final void printOne(){
+        System.out.println(1);
+    }
+}
+
+class MySystem {
+    public static final MyPrintStream MyOut = new MyPrintStream();
+
+}
