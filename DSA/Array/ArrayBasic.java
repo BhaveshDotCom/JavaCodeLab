804fa64e71d0bfcbf4fc8fb04adf0578eb04a5ec
BhaveshDotCom <mail.bhavesh.in@gmail.com>
2026-05-23 18:34:26 +0530

    feat(ArrayBasic): add for-each loop

--- DSA/Array/ArrayBasic.java
+++ DSA/Array/ArrayBasic.java
@@ -18,5 +18,11 @@ public class ArrayBasic {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
+        //  for-each loop
+        int[] numsArr = {1,2,3,4,5};
+        for (int num : numsArr){
+            System.out.print(num+" ");
+        }
+
     }
 }
